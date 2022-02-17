0cdcb49be079086302543ad5dcfbc1840d113b43
Md. Nazmul Hossain <60980415+Nazmul19285@users.noreply.github.com>
2022-02-17 14:18:07 +0600

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,81 @@
+function calculate(){
+    let income = document.getElementById("income");
+    let food = document.getElementById("food");
+    let rent = document.getElementById("rent");
+    let clothes = document.getElementById("clothes");
+    let incomeAmount = income.value;
+    let foodAmount = food.value;
+    let rentAmount = rent.value;
+    let clothesAmount = clothes.value;
+    incomeAmount = parseFloat(incomeAmount);
+    foodAmount = parseFloat(foodAmount);
+    rentAmount = parseFloat(rentAmount);
+    clothesAmount = parseFloat(clothesAmount);
+    if(Number(incomeAmount) && Number(foodAmount) && Number(rentAmount) && Number(clothesAmount)){
+        let fare = foodAmount + rentAmount + clothesAmount;
+        let expense = document.getElementById("totalExpenses");
+        expense.innerText = fare;
+        let balance = document.getElementById("balance");
+        balance.innerText = incomeAmount - fare;
+        if(fare > incomeAmount){
+            expense.style.backgroundColor = "red";
+            expense.innerText = fare + " Insufficient income";
+            balance.style.backgroundColor = "red";
+            balance.innerText = incomeAmount - fare + " Insufficient income";
+        }
+    }
+    else{
+        if(!Number(incomeAmount)){
+            wrongInput(income);
+        }
+        if(!Number(foodAmount)){
+            wrongInput(food);
+        }
+        if(!Number(rentAmount)){
+            wrongInput(rent);
+        }
+        if(!Number(clothesAmount)){
+            wrongInput(clothes);
+        }
+    }
+}
+
+function wrongInput(inputField){
+    inputField.style.backgroundColor = "red";
+    inputField.value = "Wrong input";
+}
+
+function savings(){
+    let save = document.getElementById("save");
+    let saveAmount = parseFloat(save.value);
+    if(!Number(saveAmount)){
+        save.style.backgroundColor = "red";
+        save.value = "Wrong input";
+    }
+    else{
+        let incomeAmount = document.getElementById("income").value;
+        incomeAmount = parseFloat(incomeAmount);
+        let savingsField = document.getElementById("savingsField");
+        let savingsAmount = (saveAmount/100)*incomeAmount;
+        let remainingField = document.getElementById("remainingBalance");
+        let balance = document.getElementById("balance").innerText;
+        balance = parseFloat(balance);
+        if(savingsAmount>balance){
+            savingsField.style.backgroundColor = "red";
+            remainingField.style.backgroundColor = "red";
+            savingsField.innerText = "Insufficient balance to save";
+            remainingField.innerText = "Insufficient balance to save";
+        }
+        else{
+            savingsField.style.backgroundColor = "";
+            remainingField.style.backgroundColor = "";
+            savingsField.innerText = savingsAmount;
+            remainingField.innerText = balance - savingsAmount;
+        }
+    }
+}
+
+function clearField(){
+    event.target.value = " ";
+    event.target.style.backgroundColor = "";
+}
